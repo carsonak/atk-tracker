7aeddd49b48af78cf570f43fbb0d082044d1bfc8
Andrew <49104171+carsonak@users.noreply.github.com>
2026-03-17 06:48:24 +0300

    Feat: Added event file checker

--- cmd/atk-tracker/atk-tracker.go
+++ cmd/atk-tracker/atk-tracker.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/carsonak/atk-tracker/internal/utils"
+)
+
+func main() {
+	files, err := filepath.Glob("/dev/input/event*")
+	if err != nil {
+		panic(err)
+	}
+
+	var validDevices []string
+
+	for _, filename := range files {
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		res, err := utils.IsActivityDevice(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		file.Close()
+		if res {
+			validDevices = append(validDevices, filename)
+		}
+	}
+
+	for _, dev := range validDevices {
+		fmt.Println(dev)
+	}
+}
---
+++ internal/HIDevent/HIDevent.go
@@ -0,0 +1,3 @@
+package HIDevent
+
+type Event struct{}
